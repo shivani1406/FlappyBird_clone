47a8db099fe5e5bc944bbd0b03b158b83707c53b
Shivani Sharma <shivani1406sharma@gmail.com>
2022-04-26 02:33:24 -0400

    added gameengine

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import React from 'react';
+import { GameEngine } from 'react-native-game-engine';
 
 export default function App() {
   return (
     <View style={{flex: 1}}>
-      <StatusBar style="auto" />
+      <GameEngine
+      style={{
+        position: 'absolute',top: 0,left: 0,right: 0,bottom:0
+      }}>
+        </GameEngine>
+      <StatusBar style="auto"  hidden={true}/>
     </View>
   );
 }
