f46f9ee46d4c553baacf66c5a0ab8e1a69e3645a
Shivani Sharma <shivani1406sharma@gmail.com>
2022-04-26 02:41:26 -0400

    added engine world

---
+++ entities/index.js
@@ -0,0 +1,13 @@
+import Matter from "matter-js";
+
+export default restart => {
+  let engine = Matter.Engine.create({enableSleeping: false});
+
+  let world = engine.world
+
+  engine.gravity.y = 0.4;
+
+  return {
+    physics: {engine, world}
+  }
+}
